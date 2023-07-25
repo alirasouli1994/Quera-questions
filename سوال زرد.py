75bfcd93ff1aa914827e0e2fdd997c1fe733a84f
Ali <94802372+alirasouli1994@users.noreply.github.com>
2023-07-25 16:04:30 +0330

    Create سوال زرد.py

---
+++ سوال زرد.py
@@ -0,0 +1,13 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+def wow(j):
+    n = {}
+    n=["W", "o", "w", "!"]
+
+    for i in range(j-1):
+        n.insert(1,"o")
+    
+    print("".join(n))
+
+n=int(input())
+wow(n)
