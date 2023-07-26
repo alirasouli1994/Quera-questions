b60bcdebc54c269f21b58aebf3771add77816d33
Ali <94802372+alirasouli1994@users.noreply.github.com>
2023-07-26 14:10:16 +0330

    یک سوال ساده

---
+++ یک سوال ساده.py
@@ -0,0 +1,8 @@
+# یک سوال ساده
+# https://quera.org/problemset/2885/
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+n=int(input())
+text = "man khoshghlab hastam"
+for i in range(n):
+    print(text)
