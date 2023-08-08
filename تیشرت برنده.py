39f8aee50303c7df3e78151256bdb421b0ce1e05
Ali <94802372+alirasouli1994@users.noreply.github.com>
2023-08-08 22:43:56 +0330

    Add files via upload

---
+++ تیشرت برنده.py
@@ -0,0 +1,10 @@
+# تیشرت برنده
+# https://quera.org/course/assignments/954/problems/3132
+
+a , b = map(int,input().split())
+c , d = map(int,input().split())
+
+if a >= c and b >= d :
+    print('yes')
+else :
+    print('no')
